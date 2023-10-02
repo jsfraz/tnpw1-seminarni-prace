95785014dc9a7c7452cfab3012f91b1bf4b5beed
razj <josefraz@seznam.cz>
2023-10-02 18:44:51 +0200

    Stránka kontakt.

---
+++ form.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', function(event) {
+  // zamezení defaultnímu stavu události ()
+  event.preventDefault();
+
+  // data formuláře
+  const formData = new FormData(form);
+  const formDataObject = {};
+  // naplnění dat
+  formData.forEach((value, key) => {
+    formDataObject[key] = value;
+  });
+
+  console.log(formDataObject['email'] + ': ' + formDataObject['message']);
+
+  // reset formuláře
+  form.reset();
+});
